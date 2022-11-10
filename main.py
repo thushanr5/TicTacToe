fdd8cf12c8ab1cbdbbc7a9091317b7d51ed0f15a
thushanr5 <88255435+thushanr5@users.noreply.github.com>
2022-11-10 06:38:26 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # Tic-Tac-Toe Project
+
 from array import *
 
 # board creation
 board = [['-', '-', '-', ], ['-', '-', '-', ], ['-', '-', '-', ]]
 
 
-def boardCreation():
+def board_creation():
     # iterating through the board to create a visualization of it
     for x in board:
         for y in x:
@@ -13,6 +14,7 @@ def boardCreation():
         print()
 
 
+# introduction to the game to welcome both players
 def intro():
     print(f'Welcome to the game player One, please enter your name: ')
     x_user = str(input())
@@ -23,59 +25,59 @@ def intro():
     print("Welcome to the game " + str(y_user))
 
     # create the Board after the intro
-    boardCreation()
-
-
-
-
-# def validate():
-#     while True:
-#         while board[moveX_x_coordinate] > 2 or board[moveX_x_coordinate] < 0:
-#             print('please pick a x-coordinate playerOne')
-#             moveX_x_coordinate = int(input())
-#
-#         while board[moveX_y_coordinate] > 2 or board[moveX_y_coordinate] < 0:
-#             print('please pick a y-coordinate playerOne')
-#             moveX_y_coordinate = int(input())
-#
-#         print('Your chosen board position is ' + "[" + str(moveX_x_coordinate) + "]" + "[" + str(
-#             moveX_y_coordinate) + "]")
-#
-#         board[moveX_x_coordinate][moveX_y_coordinate] = 'X'
-#
-#         boardCreation()
-
+    board_creation()
 
 
 def x_move():
     print("What is your choice of x-coordinate player One")
     moveX_x_coordinate = int(input())
 
+    # checking is move is valid in the x direction
+    while moveX_x_coordinate > 2 or moveX_x_coordinate < 0:
+        print(f'Please choose another position for the x-coordinate')
+        moveX_x_coordinate = int(input())
+
     print("What is your choice of y-coordinate player One")
     moveX_y_coordinate = int(input())
+
+    # checking is move is valid in the y direction
+    while (moveX_y_coordinate > 2 or moveX_y_coordinate < 0) or board[moveX_x_coordinate][moveX_x_coordinate] == 'O' or board[moveX_x_coordinate][moveX_y_coordinate] == 'X':
+        print(f'Please choose another position for the y-coordinate')
+        moveX_y_coordinate = int(input())
 
     print('Your chosen board position is ' + "[" + str(moveX_x_coordinate) + "]" + "[" + str(moveX_y_coordinate) + "]")
 
     board[moveX_x_coordinate][moveX_y_coordinate] = 'X'
 
-    boardCreation()
+    board_creation()
 
 
 def y_move():
     print("What is your choice of x-coordinate playerTwo")
     moveY_x_coordinate = int(input())
 
+    # checking is move is valid in the x direction
+    while moveY_x_coordinate > 2 or moveY_x_coordinate < 0:
+        print(f'Please choose another position for the x-coordinate')
+        moveY_x_coordinate = int(input())
+
     print("What is your choice of y-coordinate playerTwo")
     moveY_y_coordinate = int(input())
+
+    # checking is move is valid in the y direction
+    while (moveY_y_coordinate > 2 or moveY_y_coordinate < 0) or board[moveY_x_coordinate][moveY_y_coordinate] == 'X' or board[moveY_x_coordinate][moveY_y_coordinate] == 'O':
+        print(f'Please choose another position for the y-coordinate')
+        moveY_y_coordinate = int(input())
 
     print('Your chosen board position is ' + "[" + str(moveY_x_coordinate) + "]" + "[" + str(moveY_y_coordinate) + "]")
 
     board[moveY_x_coordinate][moveY_y_coordinate] = 'O'
 
-    boardCreation()
+    board_creation()
 
 
 def complete_game():
+    # all winnable positions -> X-player
     if ((board[0][0] == 'X' and board[0][1] == 'X' and board[0][2] == 'X') or
             (board[1][0] == 'X' and board[1][1] == 'X' and board[1][2] == 'X') or
             (board[2][0] == 'X' and board[2][1] == 'X' and board[2][2] == 'X') or
@@ -89,6 +91,7 @@ def complete_game():
         print(f'Player One Has Won Hooray! ')
         return True
 
+        # all winnable positions -> O-player
     elif ((board[0][0] == 'O' and board[0][1] == 'O' and board[0][2] == 'O') or
           (board[1][0] == 'O' and board[1][1] == 'O' and board[1][2] == 'O') or
           (board[2][0] == 'O' and board[2][1] == 'O' and board[2][2] == 'O') or
@@ -102,19 +105,13 @@ def complete_game():
         print(f'Player Two Has Won Hooray!')
         return True
 
+        # full board X or o player -> No one has won
     elif ((board[0][0] != '-' and board[0][1] != '-' and board[0][2] != '-') and
           (board[1][0] != '-' and board[1][1] != '-' and board[1][2] != '-') and
           (board[2][0] != '-' and board[2][1] != '-' and board[2][2] != '-')):
         print(f'No One Has Won the Game')
         return True
 
-
-# add conditions for if board is full or game is complete
-
-# board size
-rows = len(board)
-cols = len(board[0])
-total_size = rows * cols
 
 # play Game
 intro()
